4062f016f098fb187326b8e936e214a955a434d9
tornike001 <33287858+tornike001@users.noreply.github.com>
2017-11-13 14:52:24 +0400

    L205

--- L205/script.js
+++ L205/script.js
@@ -1,6 +1,13 @@
-var paroli = prompt("შეიყვანე: faskunji");
+for (var i = 0; i < 21; ++i) {
+	var isPrime = true;
+	for (var j =2; j <= Math.round(i / 2); ++j) {
+		if (i % j == 0) {
+			isPrime = false;
 
+		}
+	}
 
-if (paroli === "faskunji") {
-	alert("თქვენ შეხვედით");
+	if (isPrime) {
+		document.write(i + "<br>");
+	}
 }
